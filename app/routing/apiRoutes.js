5feab1b354a43783437d0dc76444c235ae56dd38
Ariana Melis <arianamelis@gmail.com>
2019-03-19 17:46:38 -0700

    updates

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -10,16 +10,29 @@ module.exports = function(app) {
     // ---------------------------------------------------------------------------
     
     app.get("/api/friends", function(req, res) {
-        res.json(friendsData);
+        res.json(friends);
       });
 
     
 // A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
-app.post("/api/friends", function(req, res) {
-    
+app.post("/:friends", function(req, res) {
+    var chosen = req.params.userData;
+    // survey results should get renderred
+    console.log(chosen)
     friends.push(req.body);
+    res.end();
 })
 };
+
+
+    // app.post("/:userData", function(req, res) {
+    //     var chosen = req.params.userData;
+    //     console.log(chosen)
+    //     friends.push(req.body);
+    //     res.end();
+    // })
+    // };
+    // friends", userData, function(data
 
 // app.post("/api/tables", function(req, res) {
 //     // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
